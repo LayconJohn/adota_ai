7a1156d4fe15e6f67cc15d0849f3a2c5e5e2d221
layconjohn <layconjohn@gmail.com>
2023-07-31 16:38:14 -0300

    feat(user): create a strong password validation

--- src/schemas/user-schema.ts
+++ src/schemas/user-schema.ts
@@ -1,10 +1,12 @@
 import joi from "joi";
 
+const pattern = /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
+
 const userSchema = joi.object({
     nome: joi.string().required(),
     email: joi.string().email().required(),
-    senha: joi.string().required(),
-    confirmarSenha: joi.string().required(),
+    senha: joi.string().regex(pattern).required(),
+    confirmarSenha: joi.string().regex(pattern).required(),
     cpf: joi.string().required()
 })
 
